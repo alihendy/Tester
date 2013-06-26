08656b7c13211827bc3e40924b8453dbfb727944
Stephen Picone (Work) <sgpicone@gmail.com>
2013-06-26 17:32:28 -0400

    POTENTIALLY SOLVED DIFFICULTY GENERATION!!
    OH MAN GUYS THIS IS REAL SHIT

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -15,28 +15,33 @@ namespace Tester
         {
             PracticeProbs pp = new PracticeProbs();
             pp.printYo();
-            Console.WriteLine("\n"+ pp.nthOfFib(100));
+            Console.WriteLine("\n" + pp.nthOfFib(100));
+            for (int i = 0; i < 10; i++)
+            {
 
-            //while (13 != 27)
-            //{
-            //    Console.WriteLine("getting value for 125 points (easy)");
-            //    generateProblem();
-            //    Console.WriteLine(totalPoints);
-            //    //double num = Convert.ToDouble(new DataTable().Compute("2 / 3", null).ToString());
-            //    //Console.WriteLine("2/3 = " + num);
-            //    //Console.WriteLine("num % 1 = " + num % 1 + "\n");
-            //    //num = Convert.ToDouble(new DataTable().Compute("-2 / 3", null).ToString());
-            //    //Console.WriteLine("-2/3 = " + num);
-            //    //Console.WriteLine("num % 1 = " + num % 1 + "\n");
-            //    //num = Convert.ToDouble(new DataTable().Compute("4 / 2", null).ToString());
-            //    //Console.WriteLine("4/2 = " + num);
-            //    //Console.WriteLine("num % 1 = " + num % 1 + "\n");
-            //    //num = Convert.ToDouble(new DataTable().Compute("-4 / 2", null).ToString());
-            //    //Console.WriteLine("-4/2 = " + num);
-            //    //Console.WriteLine("num % 1 = " + num % 1 + "\n");
-            //    allowedPoints = Convert.ToInt32(Console.ReadLine());
-            Console.ReadKey();
-            //}
+
+                smtest.generateProblem();
+
+                //while (13 != 27)
+                //{
+                //    Console.WriteLine("getting value for 125 points (easy)");
+                //    generateProblem();
+                //    Console.WriteLine(totalPoints);
+                //    //double num = Convert.ToDouble(new DataTable().Compute("2 / 3", null).ToString());
+                //    //Console.WriteLine("2/3 = " + num);
+                //    //Console.WriteLine("num % 1 = " + num % 1 + "\n");
+                //    //num = Convert.ToDouble(new DataTable().Compute("-2 / 3", null).ToString());
+                //    //Console.WriteLine("-2/3 = " + num);
+                //    //Console.WriteLine("num % 1 = " + num % 1 + "\n");
+                //    //num = Convert.ToDouble(new DataTable().Compute("4 / 2", null).ToString());
+                //    //Console.WriteLine("4/2 = " + num);
+                //    //Console.WriteLine("num % 1 = " + num % 1 + "\n");
+                //    //num = Convert.ToDouble(new DataTable().Compute("-4 / 2", null).ToString());
+                //    //Console.WriteLine("-4/2 = " + num);
+                //    //Console.WriteLine("num % 1 = " + num % 1 + "\n");
+                //    allowedPoints = Convert.ToInt32(Console.ReadLine());
+                Console.ReadKey();
+            }
         }
     }
 }
--- Tester/smtest.cs
+++ Tester/smtest.cs
@@ -2,100 +2,138 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Tester
 {
-    class smtest
+    public class smtest
     {
-        const int basic = 125;
-        static int allowedPoints = 125;
-        static int totalPoints = 0;
-        static int easyLoBound = 0, easyHiBound = 101;
-        static int easyLoOp = 0, easyHiOp = 2, ValBound = 2, ValDev = 0;
+        public const int basic = 500;
+        public static int allowedPoints = 500;
+        public static int totalPoints = 0;
+        public static int easyLoBound = 0, easyHiBound = 101;
+        public static int easyLoOp = 0, easyHiOp = 2, ValBound = 2, ValDev = 0;
 
-        static int getNumValues()
+        public static int getNumValues()
         {
             Console.WriteLine("in getNumValues");
             int numVals = 2;
-            
-
+            int numValPoints = basic / 5; //not to exceed 20% of the total difficulty
+            int maxNumVals = numValPoints / 10 + 1;
+            numVals = new Random().Next(numVals, maxNumVals);
+            int points = numVals * 5;
+            allowedPoints -= points;
+            Console.WriteLine("numval points = " + points);
             return numVals;
         }
 
-        static char[] getOperators(int numVals)
+        public static char[] getOperators(int numVals)
         {
             Console.WriteLine("in getOps");
             int numOps = numVals - 1;
             char[] ops = new char[numOps];
             int opPoints = 0;
-            int totalOpPoints = 0;
+            int maxAllowedOpPoints = basic / 5;
             int rando = 0;
             char op = '+';
             for (int i = 0; i < numOps; i++)
             {
-                rando = new Random().Next(0,allowedPoints/4+1);
-                switch (rando%4)
+                rando = new Random().Next(0,maxAllowedOpPoints);
+                if (rando < 20)
                 {
-                    case 0: op = '+'; break;
-                    case 1: op = '-'; break;
-                    case 2: op = '*'; break;
-                    case 3: op = '/'; break;
+                    op = '+'; 
                 }
-                totalOpPoints += (rando+1)*10;
-                allowedPoints -= (rando+1)*10;
+                else if (rando >= 20 && rando < 40)
+                {
+                    op = '-';
+                }
+                else if (rando >= 40 && rando < 80)
+                {
+                    op = '*';
+                }
+                else
+                {
+                    op = '/';
+                }
+                opPoints += rando;
+                maxAllowedOpPoints -= rando / 2;
+                //switch (rando%4)
+                //{
+                //    case 0: op = '+'; break;
+                //    case 1: op = '-'; break;
+                //    case 2: op = '*'; break;
+                //    case 3: op = '/'; break;
+                //}
                 ops[i] = op;
+                Thread.Sleep(100);
             }
-            totalPoints += totalOpPoints;
+            allowedPoints -= opPoints;
+            Console.WriteLine("oppoints = " + opPoints);
             
             return ops;
         }
 
-        static int[] getValue(int numVals)
+        public static int[] getValue(int numVals)
         {
             Console.WriteLine("in getValue");
             Console.WriteLine(numVals);
-            int[] vals = new int[numVals];
+            int maxAllowedPoints = (basic / 5);
+            int maxAllowedValue;
             int totalValuePoints = 0;
-            int valuePoints = 0;
-            int hiBound, loBound;
-            int value = new Random().Next(0, (allowedPoints/numVals)+1);
+
+            if (maxAllowedPoints <= 25)
+                maxAllowedValue = 51;
+            else if (maxAllowedPoints > 25 && maxAllowedPoints <= 35)
+                maxAllowedValue = 76;
+            else if (maxAllowedPoints > 35 && maxAllowedPoints <= 55)
+                maxAllowedValue = 101;
+            else if (maxAllowedPoints > 55 && maxAllowedPoints <= 75)
+                maxAllowedValue = 126;
+            else
+                maxAllowedValue = 151;
+
+            int[] vals = new int[numVals];
+            int value;
             for (int i = 0; i < numVals; i++)
             {
-                value = new Random().Next(0, allowedPoints/numVals);
-                valuePoints = value;
+                value = new Random().Next(0, maxAllowedValue);
                 //Console.WriteLine(value);
-                
-                totalValuePoints += valuePoints;
-                allowedPoints -= valuePoints;
+                totalValuePoints += Math.Abs(value / 10);
                 vals[i] = value;
+                maxAllowedValue -= Math.Abs(value / 10);
+                Thread.Sleep(100);
             }
             //Console.WriteLine("points = " + totalPoints);
             //Console.WriteLine("valuepoints = " + totalValuePoints);
             //Console.WriteLine("points - valuePoints = " + (totalPoints - totalValuePoints));
             //Console.WriteLine("points*.30 = " + (totalPoints * .30));
-            totalPoints += totalValuePoints;
+            allowedPoints -= totalValuePoints;
+            Console.WriteLine("value points = " + totalValuePoints);
             return vals;
         }
 
-        static void generateProblem()
+        public static void generateProblem()
         {
-            int numVals = getNumValues();
-            int[] values = getValue(numVals);
-            char[] ops = getOperators(numVals);
-            while (totalPoints >= basic + 50 || totalPoints <= basic-50)
-            {
-                totalPoints = 0;
+            int numVals;
+            int[] values;
+            char[] ops;
+            totalPoints = 0;
+            allowedPoints = 500;
+            //while (totalPoints >= basic + 50 || totalPoints <= basic-50)
+            //{
                 numVals = getNumValues();
                 values = getValue(numVals);
                 ops = getOperators(numVals);
-            }
+                totalPoints = basic - allowedPoints;
+            //}
             Console.Write(values[0]);
             for (int i = 0; i < ops.Length; i++)
             {
                 Console.Write(" " + ops[i] + " " + values[i + 1]);
             }
             Console.WriteLine();
+            Console.WriteLine("Total points = " + totalPoints);
 
         }
 
